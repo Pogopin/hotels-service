2762635fb8e7b923bdee9fc08ac2f5900f4c6707
Pogopin <chegarakafree@gmail.com>
2023-04-12 19:58:51 +0300

    fix code

--- src/utils/database.js
+++ src/utils/database.js
@@ -15,35 +15,37 @@ class DatabaseService {
     }
     async getSavedHotels() {
         //--------Get list hotels in Real Time--------------
+        const hotelsStore = useHotelsStore();
         onSnapshot(collection(this.db, 'hotels'), (querySnapshot) => {
-            const hotelsStore = useHotelsStore();
-            hotelsStore.hotelsList = []
-            
+            const fbHotels = [];
             querySnapshot.forEach((doc) => {
-                this.getFirebaseStorage(doc.data(), hotelsStore, doc.id)
+                const hotelInfo = {
+                    id: doc.id,
+                    name: doc.data().name,
+                    city: doc.data().city,
+                    country: doc.data().country,
+                    addres: doc.data().addres,
+                    distance: doc.data().distance,
+                    price: doc.data().price,
+                    rating: doc.data().rating,
+                    stars: doc.data().stars,
+                    // img: 'url'
+                    img: getDownloadURL(ref(this.storage, `${doc.data().img}`)).then((url) => {return url}).catch((error) => console.log(error))
+                    
+                };
+                fbHotels.push(hotelInfo);
             });
+            hotelsStore.hotelsList = fbHotels;
+            console.log(fbHotels)
+            // console.log(hotelsStore.hotelsList)
         });
         //--------------------------------------------------
     }
-    async getFirebaseStorage (data, hotelsStore, id) {// метод для получения URL из Firebase Storage
+    async getFirebaseStorage (data) {// метод для получения URL из Firebase Storage
         // const storage = getStorage();
         await getDownloadURL(ref(this.storage, `${data.img}`))
             .then((url) => {
-                const hotelInfo = {
-                        id: id,
-                        name: data.name,
-                        city: data.city,
-                        country: data.country,
-                        addres: data.addres,
-                        distance: data.distance,
-                        price: data.price,
-                        rating: data.rating,
-                        stars: data.stars,
-                        img: url
-                        
-                        
-                    };
-                    hotelsStore.hotelsList.push(hotelInfo)
+                console.log(url)
             })
             .catch((error) => {
                 console.log(error)
