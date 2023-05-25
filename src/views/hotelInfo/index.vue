7d49f710c37bbaa96be49ea3fb6595896b57c853
Sergey Kovalchuk <81366846+SergeyKovalchuk2086@users.noreply.github.com>
2023-05-25 11:17:36 +0700

    поправил отступы

--- src/views/hotelInfo/index.vue
+++ src/views/hotelInfo/index.vue
@@ -1,119 +1,114 @@
 <template>
-    <section class="ftco-section ftco-degree-bg">
-      <div class="container">
-        <div class="row">
-            <Sidebar
-            />
-            <div class="col-lg-9">
-              <div v-if="hotelInfo">
-                <h1 class="hotel-name">Отель -- {{hotelInfo.name}}</h1>
-                <img :src="hotelInfo.fullPath" alt="image">
-                <h4 class="hotel-title">Доступные номера отеля</h4>
-                <div class="numbers">
-                  <div class="number__content"
-                    v-for="num in hotelInfo.numbers" :key="num.name"
-                  >
-                    <div class="numbers__content-item item-content">
-                      <div class="item-content-slider">                        
-                        <SwiperSlider
-                          @click="sliderClick(num)"
-                        >                          
-                          <template #default>
-                            <swiper-slide                              
-                              v-for="(slide) in num.img" :key="slide.photo">
-                              <div>
-                                <img :src="slide.photoImgUrl" alt="" />
-                              </div>
-                            </swiper-slide>                            
-                          </template>                          
-                        </SwiperSlider>
-                      </div>
-                      <div class="item-content-info info">
-                        <h5 class="info-hotel-name">{{num.name}}</h5>
-                        <p>2 отдельные кровати</p>
-                        <div class="item-content-qualities">
-                          <p>Площадь:</p>
-                          <p class="item-content-closet">Шкаф</p>
-                          <p class="item-content-conditioner">Кондиционер:</p>
-                          <p class="item-content-safe">Сейф:</p>
-                        </div>
-                      </div>
-                      <div class="item-content-price">
-                          <span>Цена: {{num.price}}<p>за ночь для 2 гостей</p></span>
-                      </div>                      
-                      
-                    </div>
-                    <p class="numbers__content-description">{{num.description}}</p>
-                    <p class="numbers__content-info">{{num.info}}</p>
-
-                    <div class="form-group">
-                      <BaseButton
-                        class="booking-btn"
-                        text="Забронировать"
-                        modifyStyle="booking-button"
-                      />
-                    </div>
-                  </div>                
-                </div>
-                
-              </div>
-              <h1 v-else>Данные загружаются</h1>              
-							<!-- {{hotelInfo}} -->              							
-            </div>
-        </div>
-        <!-- В попап передаем реактивную переменную inPopupSlides со слайдами, полученной из функции sliderClick-->
-        <Popup
-          v-show="isPopupVisible"
-        >
-          <SwiperSlider
-            :isPrevArrowVisible="true"
-            :isNextArrowVisible="true"
-          >
-            <template #default>
-              <swiper-slide                                    
-                v-for="(slide) in inPopupSlides" :key="slide.photo">
-                  <div>
-                    <img :src="slide.photoImgUrl" alt="" />
+  <section class="ftco-section ftco-degree-bg">
+    <div class="container">
+      <div class="row">
+        <Sidebar />
+        <div class="col-lg-9">
+          <div v-if="hotelInfo">
+            <h1 class="hotel-name">Отель -- {{ hotelInfo.name }}</h1>
+            <img :src="hotelInfo.fullPath" alt="image" />
+            <h4 class="hotel-title">Доступные номера отеля</h4>
+            <div class="numbers">
+              <div
+                class="number__content"
+                v-for="num in hotelInfo.numbers"
+                :key="num.name"
+              >
+                <div class="numbers__content-item item-content">
+                  <div class="item-content-slider">
+                    <SwiperSlider @click="sliderClick(num)">
+                      <template #default>
+                        <swiper-slide
+                          v-for="slide in num.img"
+                          :key="slide.photo"
+                        >
+                          <div>
+                            <img :src="slide.photoImgUrl" alt="" />
+                          </div>
+                        </swiper-slide>
+                      </template>
+                    </SwiperSlider>
                   </div>
-              </swiper-slide>                                                 
-            </template>
-          </SwiperSlider>
-        </Popup>
-
+                  <div class="item-content-info info">
+                    <h5 class="info-hotel-name">{{ num.name }}</h5>
+                    <p>2 отдельные кровати</p>
+                    <div class="item-content-qualities">
+                      <p>Площадь:</p>
+                      <p class="item-content-closet">Шкаф</p>
+                      <p class="item-content-conditioner">Кондиционер:</p>
+                      <p class="item-content-safe">Сейф:</p>
+                    </div>
+                  </div>
+                  <div class="item-content-price">
+                    <span
+                      >Цена: {{ num.price }}
+                      <p>за ночь для 2 гостей</p></span
+                    >
+                  </div>
+                </div>
+                <p class="numbers__content-description">
+                  {{ num.description }}
+                </p>
+                <p class="numbers__content-info">{{ num.info }}</p>
+                <div class="form-group">
+                  <BaseButton
+                    class="booking-btn"
+                    text="Забронировать"
+                    modifyStyle="booking-button"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <h1 v-else>Данные загружаются</h1>
+          <!-- {{hotelInfo}} -->
+        </div>
       </div>
-    </section>
+      <!-- В попап передаем реактивную переменную inPopupSlides со слайдами, полученной из функции sliderClick-->
+      <Popup v-show="isPopupVisible">
+        <SwiperSlider :isPrevArrowVisible="true" :isNextArrowVisible="true">
+          <template #default>
+            <SwiperSlide v-for="slide in inPopupSlides" :key="slide.photo">
+              <div>
+                <img :src="slide.photoImgUrl" alt="" />
+              </div>
+            </SwiperSlide>
+          </template>
+        </SwiperSlider>
+      </Popup>
+    </div>
+  </section>
 </template>
 <script setup>
-import { BaseButton } from '@/components/ui';
-import { Swiper, SwiperSlide } from 'swiper/vue';
-import { Sidebar, Popup } from '@/components/widgets';
-import { SwiperSlider } from '@/components/widgets';
-import { defineProps, computed, onBeforeMount, onMounted, ref } from 'vue';
-import { useRoute } from 'vue-router';
-import { useHotelsStore } from '../../stores/hotelsStore.js';
+import { BaseButton } from "@/components/ui";
+import { Sidebar, Popup } from "@/components/widgets";
+import { SwiperSlide } from "swiper/vue";
+import { SwiperSlider } from "@/components/widgets";
+import { computed, onMounted, ref } from "vue";
+import { useRoute } from "vue-router";
+import { useHotelsStore } from "../../stores/hotelsStore.js";
 
 // здесь получаем через getter из store данные об отеле!
 const hotelsStore = useHotelsStore();
-const hotelInfo = computed(()=> hotelsStore.getHotelInfo);
+const hotelInfo = computed(() => hotelsStore.getHotelInfo);
 const route = useRoute();
-const id = computed(()=>  route.params.id);
+const id = computed(() => route.params.id);
 
 const isPopupVisible = ref(false);
 const inPopupSlides = ref([]);
 
-function sliderClick (sl) {
+function sliderClick(sl) {
   isPopupVisible.value = true;
   inPopupSlides.value = sl.img;
 }
-onMounted(()=> {
-    window.addEventListener('click', (event) => {
-        if (event.target.closest('.popup-close-btn')) isPopupVisible.value = false;
-    });        
-    hotelsStore.fetchHotelById(id.value);
-})
+onMounted(() => {
+  window.addEventListener("click", (event) => {
+    if (event.target.closest(".popup-close-btn")) isPopupVisible.value = false;
+  });
+  hotelsStore.fetchHotelById(id.value);
+});
 // const hotelsList = computed(()=> hotelsStore.getHotelsData);
 // const hotelInfo = computed(()=> hotelsList.value.find(hotel => hotel.id === id.value));
-
 </script>
 <style scoped>
 .booking-btn {
@@ -164,32 +159,31 @@ onMounted(()=> {
   border-radius: 3px;
   padding: 0 5px 0;
   position: relative;
-  
 }
 .numbers__content-info {
   margin-bottom: 20px !important;
 }
 .item-content-closet::before {
-  content: '';
+  content: "";
   position: absolute;
   left: -23px;
-  background-image: url('@/assets/icons/closet.svg');
+  background-image: url("@/assets/icons/closet.svg");
   width: 20px;
   height: 20px;
 }
 .item-content-conditioner::before {
-  content: '';
+  content: "";
   position: absolute;
   left: -23px;
-  background-image: url('@/assets/icons/conditioner.svg');
+  background-image: url("@/assets/icons/conditioner.svg");
   width: 20px;
   height: 20px;
 }
 .item-content-safe::before {
-  content: '';
+  content: "";
   position: absolute;
   left: -23px;
-  background-image: url('@/assets/icons/safe.svg');
+  background-image: url("@/assets/icons/safe.svg");
   width: 20px;
   height: 20px;
 }
