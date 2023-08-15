10b48c586eeed79ff1a0556e01edd4eb3b29d7a3
Pogopin <chegarakafree@gmail.com>
2023-08-15 15:08:20 +0300

    Updates

--- assets/AppLayoutDefault-f27547cb.js
+++
@@ -1,1 +0,0 @@
-import{d as a,_ as t,o,c as r,a as s,u as n,r as _,F as c}from"./index-78c671a9.js";const l=a(()=>t(()=>import("./index-b4ff3b44.js"),["assets/index-b4ff3b44.js","assets/index-78c671a9.js","assets/index-b0476ea5.css"])),m={__name:"AppLayoutDefault",setup(u){return(e,p)=>(o(),r(c,null,[s(n(l)),_(e.$slots,"default")],64))}};export{m as default};
